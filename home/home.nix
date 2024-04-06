d3bc27a04b8636c61e2e723d0ab1705bd73663b3
alexpaniman <alexpaniman@gmail.com>
2024-04-07 01:10:36 +0300

    feature: use emacs nix overlay so that vterm works

--- home/home.nix
+++ home/home.nix
@@ -163,10 +163,29 @@ rec {
 
   programs.command-not-found.enable = false;
 
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs;
-  };
+  programs.emacs =
+    let
+      emacs-overlay = import (fetchTarball {
+        url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+        sha256 = "1jppksrfvbk5ypiqdz4cddxdl8z6zyzdb2srq8fcffr327ld5jj2";
+      });
+      my-emacs = pkgs.emacs.override {
+        withNativeCompilation = true;
+        withSQLite3 = true;
+        withTreeSitter = true;
+        withWebP = true;
+      };
+      my-emacs-with-packages = (pkgs.emacsPackagesFor my-emacs).emacsWithPackages (epkgs: with epkgs; [
+        vterm
+        multi-vterm
+        pdf-tools
+        treesit-grammars.with-all-grammars
+      ]);
+    in
+    {
+      enable = true;
+      package = my-emacs-with-packages;
+    };
 
   programs.alacritty = {
     enable = true;
