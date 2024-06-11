852392f9a1eb2397db0f8e41bf39ac889f215ff3
alexpaniman <alexpaniman@gmail.com>
2024-06-11 20:28:11 +0300

    adjustment: downgrade from unstable to nixos-24.05

--- conf/flake.nix
+++ conf/flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nix-alien.url = "github:thiagokokada/nix-alien";
 
     # home-manager = {
