1eb20028feabf30a3c58986616778d3d04fd6f06
alexpaniman <alexpaniman@gmail.com>
2024-06-17 14:05:20 +0300

    revert "feature: bleeding edge emacs using emacs-overlay"
    Reverting because emacs-30.05 breaks compatibility with ts-modes.
    This reverts commit bd5a7f554cb787de98501a11cc364f164660c89a.

--- home/flake.nix
+++ home/flake.nix
@@ -8,17 +8,12 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = { nixpkgs, home-manager, emacs-overlay, ... }:
+  outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ emacs-overlay.overlays.default ];
-      };
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."alex" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
