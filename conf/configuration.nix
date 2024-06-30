344e323ccdb3f88ef5e9519586731d5b6ccfa746
alexpaniman <alexpaniman@gmail.com>
2024-06-30 18:53:38 +0300

    feature: polkit gui

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -337,6 +337,31 @@
     libGL
   ];
 
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+        polkit.addRule(function(action, subject) {
+            if (action.id == "org.freedesktop.policykit.exec" && subject.isInGroup("wheel")) {
+                return polkit.Result.YES;
+            }
+        });
+    '';
+  };
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   services.ollama = {
     enable = true;
     acceleration = "cuda";
